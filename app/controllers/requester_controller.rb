aa6c53091afe7847cff4821ac4d21d92c121aad5
Kubilay Caglayan <kblycaglayan@gmail.com>
2021-07-11 12:51:30 +0300

    Allow POST requests

--- app/controllers/requester_controller.rb
+++ app/controllers/requester_controller.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class RequesterController < ApplicationController
   skip_before_action :verify_authenticity_token
@@ -8,28 +9,40 @@ class RequesterController < ApplicationController
       render status: 400
     else
       path = insert_forward_slash request.fullpath.dup
+      response = make_a_request path, request
 
-      response = HTTParty.get(path)
-
-      if (response.code == 200)
+      if response.code == 200
         render json: response.parsed_response
       else
-        render plain: "Error in request", status: response.code
+        render plain: 'Error in request', status: response.code
       end
     end
   end
 
   private
 
-  def requester_params
-    params
+  def make_a_request(path, request)
+    if request.method == 'GET'
+      HTTParty.send(request.method.downcase, path)
+    else
+      HTTParty.send(
+        request.method.downcase,
+        path,
+        body: accept_all_params[:requester].to_json,
+        headers: { 'Content-Type': 'application/json' }
+      )
+    end
+  end
+
+  def accept_all_params
+    params.permit!
   end
 
   def is_authorized
     return true
 
     # FOR OPTIONAL AUTH
-    if params['key'] == "0101"
+    if params['key'] == '0101'
       return true
     else
       return false
@@ -39,6 +52,6 @@ class RequesterController < ApplicationController
   def insert_forward_slash path
     index_of_fs = path[1, path.length].index(/\//)
 
-    path[1, path.length].insert(index_of_fs, "/")
+    path[1, path.length].insert(index_of_fs, '/')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get '*path', to: 'requester#get', headers: :any
+  match '*path', to: 'requester#get', via: [:get, :post], headers: :any
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
